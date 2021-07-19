28c524060b102ddaa7f3e3c8c79ab6733919551d
Eder Carlos Fernandes <eder.tecnologias@gmail.com>
2021-07-19 20:47:18 -0300

    Add files via upload

---
+++ Aula_17_4/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+    for(a = 0, b = 10; a < b; a++, b--){
+        printf("A = %d e b = %d \n",a, b);
+    }
+}
---
+++ aula_17_2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Digite dois valores inteiros!\n");
+    scanf("%d %d", &a, &b);
+    for(c = a; c <= b; c--){
+        printf("Contando %d \n",c);
+    }
+}
